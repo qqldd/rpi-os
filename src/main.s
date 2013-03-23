cc18ef6f92e16719d30d8e7c4c746d8215a7d399
Myrice <qqlddg@gmail.com>
2013-03-22 21:18:32 -0700

    L2: Blink ACT LED
    Signed-off-by: Myrice <qqlddg@gmail.com>

--- src/main.s
+++ src/main.s
@@ -2,11 +2,35 @@
 .globl _start
 _start:
 ldr r0,=0x20200000
+
+# Open 16th GPIO 
 mov r1,#1
 lsl r1,#18
 str r1,[r0,#4]
+
 mov r1,#1
 lsl r1,#16
-str r1,[r0,#40]
+
 loop$: 
+
+# Open ACT LED
+str r1,[r0,#40]
+
+# Wait
+mov r2,#0x3F0000
+wait1$:
+sub r2,#1
+cmp r2,#0
+bne wait1$
+
+# Close ACT LED
+str r1,[r0,#28]
+
+# Wait
+mov r2,#0x3F0000
+wait2$:
+sub r2,#1
+cmp r2,#0
+bne wait2$
+
 b loop$
